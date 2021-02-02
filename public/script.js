c83a47a414369d66823bd1fe90044473a2ae0b70
Dennis Xiong <xion1509@umn.edu>
2021-02-01 23:37:15 -0600

    added more CSS styling

--- public/script.js
+++ public/script.js
@@ -1,18 +1,62 @@
+const socket = io('/');
 const videoGrid = document.getElementById('video-grid');
-console.log(videoGrid);
 const myVideo = document.createElement('video');
 myVideo.muted = true;
 
+var peer = new Peer(undefined, {
+    path: '/peerjs', 
+    host: '/', 
+    port: '3030'
+});
+
 const getMedia = async () => {
     try {
-        const stream = await navigator.mediaDevices.getUserMedia({audio: true, video: true});
-        return stream;
+        return await navigator.mediaDevices.getUserMedia({audio: true, video: true});    
     }
     catch(e) {
         console.log('error', e);
     }
 }
 
+
+getMedia().then(stream => {
+    addVideoStream(myVideo, stream);
+    // Answer call from new user
+    peer.on('call', call => {
+        console.log('answering call from new user');
+        call.answer(stream);
+        const video = documnent.createElement('video');
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream);
+        })
+    })
+
+    // Pass stream to socket
+    // Clients receive notif that a new user has joined the room
+    socket.on('user-connected', (userId) => { // userId is new user
+        console.log('new user is ', userId);
+        connectToNewUser(userId, stream);
+})
+})
+
+// Open peer connections
+peer.on('open', id => {
+    // Send to server that a user has joined room
+    console.log('A user has joined');
+    socket.emit('join-room', ROOM_ID, id);
+
+})
+
+const connectToNewUser = (userId, stream) => {
+    console.log(`new user ${userId} joined`);
+    // Call other user using peer
+    var call = peer.call(userId, stream);
+    const video = document.createElement('video'); // create video for the other user and add them to your stream
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream);
+    })
+}
+// Connect video element with stream 
 const addVideoStream = (video, stream) => {
     video.srcObject = stream;
     video.addEventListener('loadedmetadata', () => {
@@ -20,7 +64,3 @@ const addVideoStream = (video, stream) => {
     })
     videoGrid.append(video);
 }
-
-getMedia().then(stream => {
-    addVideoStream(myVideo, stream);
-})
--- server.js
+++ server.js
@@ -3,12 +3,23 @@ const app = express();
 const server = require('http').Server(app);
 const { v4: uuidv4} = require('uuid');
 
+// socket.io
+const io = require('socket.io')(server);
 app.set('view engine', 'ejs');
+
+// Peer-to-Peer
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
 
 // use static files such as javascript and CSS
 app.use(express.static('public'));
 
-// Redirect 
+// use Peer-to-Peer
+app.use('/peerjs', peerServer);
+
+// Redirect to Discord room
 app.get('/', (req, res) => {
     res.redirect(`/${uuidv4()}`);
 })
@@ -18,7 +29,13 @@ app.get('/:room', (req, res) => {
     res.render('room', { roomId: req.params.room });
 })
 
-
-server.listen(3000, (req, res) => {
-    console.log("listening to port 3000");
+// Listen to socket 
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId); // connect socket to room
+        socket.to(roomId).broadcast.emit('user-connected', userId); // Alert users that someone has joined the room
+    })
+})
+server.listen(3030, (req, res) => {
+    console.log("listening to port 3030");
 });
